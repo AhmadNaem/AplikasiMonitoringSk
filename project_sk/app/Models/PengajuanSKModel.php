c10324af169485849841bbab0de79392f3838d63
2201020123_HANDICAP <153718139+Handicap211002@users.noreply.github.com>
2024-12-20 00:03:03 +0700

    Update PengajuanSKModel.php

--- project_sk/app/Models/PengajuanSKModel.php
+++ project_sk/app/Models/PengajuanSKModel.php
@@ -6,11 +6,11 @@ use CodeIgniter\Model;
 
 class PengajuanSkModel extends Model
 {
-    protected $table = 'pengajuan';
-    protected $primaryKey = 'id_sk';
+    protected $table = 'melihat_status' ;
+    protected $primaryKey = 'id_status';
     protected $allowedFields = [
-        'id_pengaju', 'judul_sk', 'tanggal_pengajuan', 'tembusan', 'menimbang',
-        'menetapkan', 'memperhatikan', 'mengingat'
+        'id_status', 'id_sk', 'id_pimpinan', 'id_pengaju', 'id_staff',
+        'tanggal_update'
     ];
     protected $useTimestamps = true;
     public function getByPengaju($id_pengaju)
@@ -22,4 +22,5 @@ class PengajuanSkModel extends Model
 {
     return $this->db->table('pengajuan')->where('id_sk', $id_sk)->update(['status' => $status]);
 }
+
 }
